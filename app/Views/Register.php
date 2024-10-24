386928ab6c767e41c6eda814a3bd964c0b716f00
DanielMichel305 <daniel2202556@miuegypt.edu.eg>
2024-10-24 22:49:05 +0300

    tweaked file includes based on previous push

--- app/Views/Register.php
+++ app/Views/Register.php
@@ -7,6 +7,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
     <link rel="stylesheet" href="/css/base.css">
     <link rel="stylesheet" href="/css/register.css">
+
+
 </head>
 <body>
 <?php include 'navbar.php'; ?>
--- app/Views/navbar.php
+++ app/Views/navbar.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AI-Powered Essay Grading System</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-    <link rel="stylesheet" href="/css/navbar.css">   
+    <link rel="stylesheet" href="/css/navbar.css">
+   
 </head>
 <body>
 <header>
